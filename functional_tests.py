5ff22eab8d935f3c426c796bd47a78f92b929161
Joel Ginsberg <j@joelginsberg.com>
2018-10-31 21:52:59 -0500

    added To-do list item input check to FT

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest (unittest.TestCase):
@@ -17,29 +19,43 @@ class NewVisitorTest (unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do list item immediately
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-# She types "buy peacock feathers" into a text box
-# (Edith's hobby is tying fly-fishing lures
+        # She types "buy peacock feathers" into a text box
+        # (Edith's hobby is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-# When she hits enter, the page updates and now the page lists:
-# "1: Buy peacock feathers" as an item in a todo list
+        # When she hits enter, the page updates and now the page lists:
+        # "1: Buy peacock feathers" as an item in a todo list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-# There is still a text box inviting her to add another item
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+        # There is still a text box inviting her to add another item
+        # She enters "Use peacock feathers to make a fly"
+        self.fail('Finish the test!')
 
-# She enters "Use peacock feathers to make a fly"
+        # The page updates again, and now shows both items on her list
 
-# The page updates again, and now shows both items on her list
+        # Edith wonders whether the site will remember her list. Then she
+        # sees the site has generated a unique URL for her -- There is some
+        # explanatory text to that effect.
 
-# Edith wonders whether the site will remember her list. Then she
-# sees the site has generated a unique URL for her -- There is some
-# explanatory text to that effect.
+        # She visits that URL - her to-do list is still there
 
-# She visits that URL - her to-do list is still there
-
-# Satisfied, she goes back to sleep
+        # Satisfied, she goes back to sleep
 
 if __name__ == '__main__':
     unittest.main()
